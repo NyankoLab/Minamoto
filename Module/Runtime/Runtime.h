91e6714227df1859827ede6f234aab7328e10ca4
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2025-08-13 01:10:19 +0800

    Update

--- Module/Runtime/Runtime.h
+++ Module/Runtime/Runtime.h
@@ -61,14 +61,14 @@ constexpr uint32_t operator ""_cc(char const* text, size_t length)
 {
     uint32_t value = 0;
     for (size_t i = 0; i < length; ++i)
-        value += uint32_t(uint8_t(text[i])) << (i * 8);
+        value += uint32_t(uint8_t(text[i])) << (i % 4 * 8);
     return value;
 };
 constexpr uint64_t operator ""_CC(char const* text, size_t length)
 {
     uint64_t value = 0;
     for (size_t i = 0; i < length; ++i)
-        value += uint64_t(uint8_t(text[i])) << (i * 8);
+        value += uint64_t(uint8_t(text[i])) << (i % 8 * 8);
     return value;
 };
 
